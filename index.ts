00ff20183134d038f75818e179823f0ac52399ec
Dmitry <d@kozlov.ru>
2025-04-30 23:56:20 +0700

    Added authorization

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+import { sign } from './keychain'
 import type { Sender } from 'channel/channel'
 import { Channel } from 'channel/client'
 
 const defaultHubPort = Number(Bun.env.HUBPORT ?? 1997)
+const v = '0'
 export class Client {
   channel = new Channel()
   sender: Sender
   constructor(port: number = defaultHubPort) {
-    this.sender = this.channel.connect(port)
+    this.sender = this.channel.connect(port, {
+      headers: async () => ({ auth: await sign(), v }),
+    })
   }
   post(path: string, body?: any) {
     return this.sender.send(path, body)
